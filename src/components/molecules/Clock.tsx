9fe88f3dd273f0342a3bc0187c5c2b36ae9866f0
HugoCastroBr <hugoecastro2008@hotmail.com>
2023-11-21 16:53:51 -0300

    feat: search and order start menu and animations

--- src/components/molecules/Clock.tsx
+++ src/components/molecules/Clock.tsx
@@ -26,25 +26,28 @@ const Clock = () => {
     }, 1000)
   }, [])
 
+  
+
   return (
     <>
-      {
-        isCalendarOpen
-        &&
-        <div className='absolute bottom-10 bg-white bg-opacity-80 rounded-md'>
-          <Calendar
-            renderDay={(date) => {
-              const day = date.getDate()
-              const currentDay = new Date().getDate()
-              return (
-                <Indicator size={6} color="blue" offset={-2} disabled={day !== currentDay}>
-                  <div>{day}</div>
-                </Indicator>
-              );
-            }}
-          />
-        </div>
-        }
+
+
+      <div 
+      className={`absolute bottom-10 bg-white bg-opacity-80 rounded-md w-64 h-64
+      ${isCalendarOpen ? 'bottom-10 right-2' : '-right-72'} transition-all duration-500 ease-in-out
+      `}>
+        <Calendar
+          renderDay={(date) => {
+            const day = date.getDate()
+            const currentDay = new Date().getDate()
+            return (
+              <Indicator size={6} color="blue" offset={-2} disabled={day !== currentDay}>
+                <div>{day}</div>
+              </Indicator>
+            );
+          }}
+        />
+      </div>
       <div
         onClick={() => setIsCalendarOpen(!isCalendarOpen)}
         className='w-20 cursor-pointer'>
--- src/components/system/StartMenu.tsx
+++ src/components/system/StartMenu.tsx
@@ -1,19 +1,42 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import CustomText from '../atoms/CustomText'
 import Image from 'next/image'
 import { Menu, Button, Accordion, Group } from '@mantine/core'
 import useStore from '@/hooks/useStore'
 import { WindowAddTab } from '@/store/actions'
 import { uuid } from '@/utils/file'
+import { windowStateProps } from '@/types/windows'
 
 const StartMenu = () => {
 
   const { states, dispatch } = useStore()
+  const [searchValue, setSearchValue] = React.useState('')
 
 
-  const renderPrograms = () => {
-    return states.Windows.windows.map((window, index) => {
+
+
+  const orderPrograms = () => {
+    const programsOrdered = [...states.Windows.windows].filter((v) => {
+      const program = v.title.toLowerCase()
+      const search = searchValue.toLowerCase()
+      return program.includes(search)
+    }).sort((a, b) => {
+      if (a.title > b.title) {
+        return 1;
+      }
+      if (a.title < b.title) {
+        return -1;
+      }
+      return 0;
+    });
+
+    return programsOrdered
+  }
+
+  const renderPrograms = (programsOrdered:windowStateProps[]) => {
+
+    return programsOrdered.map((window, index) => {
       return (
         <>
           <Menu.Item
@@ -50,6 +73,22 @@ const StartMenu = () => {
     })
   }
 
+  const NotFoundComponent = () => {
+    return (
+      <div className='flex flex-col justify-center items-center w-full h-full mt-4'>
+        <Image
+          src='/assets/icons/Alaska.png'
+          alt='Program Icon'
+          width={32}
+          height={32}
+        />
+        <CustomText
+          className='text-center mt-2 text-base font-semibold text-slate-600'
+          text='Program not found'
+        />
+      </div>
+    )
+  }
 
   return (
 
@@ -57,9 +96,9 @@ const StartMenu = () => {
       shadow="md"
       width={200}
       transitionProps={{
-        duration: 300,
+        duration: 500,
         timingFunction: 'ease',
-        transition: 'pop-bottom-left',
+        transition: 'slide-right',
       }}
     >
       <Menu.Target>
@@ -104,22 +143,34 @@ const StartMenu = () => {
             width: '240px',
             marginTop: '6px',
             borderRadius: '0px',
-            borderTopRightRadius: '8px',
             backdropFilter: 'blur(6px)',
             backgroundColor: 'rgba(255, 255, 255, 0.4)',
             border: '1px solid rgba(129, 233, 235, 0.2)',
             filter: 'drop-shadow(0px 0px 4px 1px rgba(26, 36, 35, 0.35))',
             boxShadow: '0px 0px 4px 1px rgba(26, 36, 35, 0.35)',
-            overflowY: 'scroll',
+            overflowY: 'auto',
+            overflowX: 'hidden',
           }
         }}
       >
         <Menu.Label>
-          <CustomText
-            text='Start Menu'
-          />
+          <div>
+            <input 
+            value={searchValue}
+            placeholder='Search...'
+            onChange={(e) => {
+              setSearchValue(e.target.value)
+            }}
+            className='w-full h-8 mt-1 outline-none border border-gray-300 rounded-md px-2' 
+            />
+          </div>
         </Menu.Label>
-        {renderPrograms()}
+        {renderPrograms(orderPrograms()).length > 0 
+        ?
+        renderPrograms(orderPrograms())
+        :
+        <NotFoundComponent/>
+        }
       </Menu.Dropdown>
     </Menu>
 
